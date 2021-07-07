bb7481fea750f1060948a9ad021e484cac087ad8
SaneStreet <MichaelHansenIT@live.dk>
2021-07-07 15:53:08 +0200

    Added redirects, catchall and some navigation

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
     <router-link :to="{ name: 'About'}">About</router-link> |
     <router-link :to="{ name: 'Jobs'}">Jobs</router-link>
   </div>
+
+  <button @click="redirect"> Redirect </button>
+  <button @click="back"> Go back </button>
+  <button @click="forward"> Go forward </button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    },
+  }
+}
+</script>
 
 <style>
 #app {
@@ -23,13 +44,20 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
 }
 
 #nav a.router-link-exact-active {
   color: black;
   background: orange;
-  text-decoration: none;
+}
+
+button {
+  margin: 0 10px;
   padding: 10px;
+  border: none;
   border-radius: 4px;
 }
 </style>
