ed97280755d46a327c3a5da01b10e5fb5bca298c
SaneStreet <MichaelHansenIT@live.dk>
2021-07-13 14:41:23 +0200

    Added data to the jobs objects

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,35 @@
 <template>
-  <h1>Job Details</h1>
+
+<div v-if="job">
+  <h1>{{ job.title }}</h1>
   <p>The job id is {{ id }}</p>
+  <p>{{ job.details }}</p>
+</div>
+<div v-else>
+  <p>Loading job details...</p>
+</div>
+  
 </template>
 
 <script>
 export default {
 	props: ['id'],
+  data () {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    // to see it on other devices than on your machine
+    // write your IPv4 address so it matches the
+    // default is localhost for json-server
+    // run 'npx json-server --watch -H localhost -P 3000 data/db.json' for localhost access only
+    // run 'npx json-server --watch -H [YourIPv4] -P 3000 data/db.json' for Networked access
+      fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    }
 }
 </script>
 
